6e8b2418999a81a35773bb50740cfe34c6950ead
Phillip Stockmann <zyanklee@gmail.com>
2026-02-07 21:13:37 +0100

    chore: Release v2.1.2

--- README.md
+++ README.md
@@ -255,7 +255,7 @@ mvn clean package
 # The compiled JAR will be in target/
 ```
 
-The built plugin will be available at `target/NetherRatio-2.1.1.jar`
+The built plugin will be available at `target/NetherRatio-2.1.2.jar`
 
 ## 🐛 Troubleshooting
 
--- src/main/java/org/doraji/netherratio/NetherRatio.java
+++ src/main/java/org/doraji/netherratio/NetherRatio.java
@@ -14,7 +14,7 @@ import org.doraji.netherratio.commands.WorldRatioCommand;
  * @author xDxRAx (Original Author)
  * @author NetherRatio Team
  * @author ZyanKLee (Maintainer)
- * @version 2.1.1
+ * @version 2.1.2
  */
 public final class NetherRatio extends JavaPlugin {
 
