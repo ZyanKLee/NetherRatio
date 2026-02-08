33f59d2ec3c8c9d23d103121b461491fa64801f8
Phillip Stockmann <zyanklee@gmail.com>
2026-02-08 22:05:06 +0100

    chore: Release v2.3.2

--- src/main/java/org/doraji/netherratio/ConfigManager.java
+++ src/main/java/org/doraji/netherratio/ConfigManager.java
@@ -17,7 +17,7 @@ import java.util.Map;
  * @author xDxRAx (Original Author)
  * @author NetherRatio Team
  * @author ZyanKLee (Maintainer)
- * @version 2.3.1
+ * @version 2.3.2
  */
 public class ConfigManager {
 
